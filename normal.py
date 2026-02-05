bf500e0910a1eb65ec68927a8e242bc295f95a76
gbq38u <gbmailp@gmail.com>
2026-02-05 09:13:03 +0300

    add plot_utils and gitignore

--- normal.py
+++ normal.py
@@ -1,10 +1,7 @@
-import matplotlib.pyplot as plt
 import numpy as np
+from plot_utils import my_plot
 
 size = 1000
-
 x = np.linspace(0, size, size)
 y = np.random.normal(0, 1, size)
-
-plt.plot(x, y)
-plt.show()
+my_plot(x, y)
